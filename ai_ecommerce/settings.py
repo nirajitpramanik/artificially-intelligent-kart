b0489c3c20ff7ee1ecbe94cafacd0cf0fd10c163
NP2215 <leojit2213@gmail.com>
2024-11-07 22:34:20 +0530

    feat: Hiding secret key

--- ai_ecommerce/settings.py
+++ ai_ecommerce/settings.py
@@ -13,6 +13,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cs$%uuyi)i_4wrnu7ils7)2d-$5#7cr0^$#%f455!!p_*uif4z'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
